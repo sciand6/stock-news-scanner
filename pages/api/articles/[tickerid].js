fbf211369875ba839585511e9a88915eb83907f9
Malcolm <sciand6@gmail.com>
2022-09-22 20:46:57 -0400

    Initial commit.

---
+++ pages/api/articles/[tickerid].js
@@ -0,0 +1,9 @@
+const { parse } = require('rss-to-json')
+
+export default async function handler(req, res) {
+  const { tickerid } = req.query
+  const response = await parse(
+    `https://feeds.finance.yahoo.com/rss/2.0/headline?s=${tickerid}&region=US&lang=en-US`,
+  )
+  return res.status(200).json(response)
+}
